44fccab69a35a084db9c9806f268529ffef7a9f0
Karen Yip <karenyip1999@gmail.com>
2024-04-02 16:52:00 +0100

    Added handleInputChange in SearchForm.jsx

--- src/components/forms/SearchForm.jsx
+++ src/components/forms/SearchForm.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
 const SearchForm = () => {
 
+    const [searchInput, setSearchInput] = useState("")
+
+    const handleInputChange = (event) => {
+        const searchItem = event.target.value;
+        setSearchInput(searchItem);
+    }
 
 
     return (
         <>
-        
+        <input 
+        type = "text"
+        value = {searchInput}
+        onChange={handleInputChange}
+        placeholder="Search..."
+         ></input>
         
         </>
     )
