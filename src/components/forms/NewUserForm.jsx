f5dd952fc1fdbc95b362cadd63efb0a02a3baa35
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-04-03 14:21:11 +0100

    Ammend title for new user form

--- src/components/forms/NewUserForm.jsx
+++ src/components/forms/NewUserForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../../styles/NewUserForm.css";
 
 const NewUserForm = ({ postUser, setCurrentUserId }) => {
   const [name, setName] = useState("");
