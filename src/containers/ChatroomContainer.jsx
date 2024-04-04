8280a4c3fc9d87d3d24142778daa760941a8600b
Karen Yip <karenyip1999@gmail.com>
2024-04-04 11:37:27 +0100

    Added comic font family to h1 tag in ChatroomContainer.css

--- src/containers/ChatroomContainer.jsx
+++ src/containers/ChatroomContainer.jsx
@@ -17,6 +17,7 @@ import ChatroomNavigation from "../components/ChatroomNavigation";
 import ChatroomMessageList from "../components/lists/ChatroomMessageList";
 import NewMessageForm from "../components/forms/NewMessageForm";
 
+
 const API_ROOT = "http://localhost:8080";
 
 const ChatroomContainer = () => {
@@ -267,7 +268,7 @@ const ChatroomContainer = () => {
 
   return (
     <>
-      <h1>Big Blether</h1>
+      <h1>BIG BLETHER</h1>
       <RouterProvider router={chatroomRoutes} />
     </>
   );
