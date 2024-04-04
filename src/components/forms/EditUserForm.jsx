20ea1ef44dbf8cd12361896fa7bc2dcdd898d6f2
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-04-04 09:31:42 +0100

    Create css file for edit user form

--- src/components/forms/EditUserForm.jsx
+++ src/components/forms/EditUserForm.jsx
@@ -32,41 +32,45 @@ const EditUserForm = ({ getCurrentUser,updateUser, deleteUser}) => {
 
 
     return (
-        <form onSubmit={handleFormSubmit}>
+    <>
+        <h4>Edit user: </h4>
+            <div>
+                <form onSubmit={handleFormSubmit}>
 
-            <h5>Current Username: {user.name}</h5>
-            <h5>Current Email: {user.email}</h5>
-            <h5>Current DateOfBirth: {user.dateOfBirth}</h5>
+                    <h5>Current Username: {user.name}</h5>
+                    <h5>Current Email: {user.email}</h5>
+                    <h5>Current DateOfBirth: {user.dateOfBirth}</h5>
+                    
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                        required
+                    />
 
-            <h4>Edit user: </h4>
-            <input
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-                required
-            />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        required
+                    />
 
-            <input
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                required
-            />
-
-            <input
-                type="date"
-                placeholder="Date of Birth"
-                value={dateOfBirth}
-                onChange={(event) => setDateOfBirth(event.target.value)}
-                required
-            // Must be in "1980-03-06" format - add to form validation
-            />
-            
-            <button onClick={handleUserDelete}>Delete</button>
-            <input type="submit" value="Edit User" />
-        </form>
+                    <input
+                        type="date"
+                        placeholder="Date of Birth"
+                        value={dateOfBirth}
+                        onChange={(event) => setDateOfBirth(event.target.value)}
+                        required
+                    // Must be in "1980-03-06" format - add to form validation
+                    />
+                    
+                    <button onClick={handleUserDelete}>Delete</button>
+                    <input type="submit" value="Edit User" />
+                </form>
+            </div>
+    </>
     );
 }
 
